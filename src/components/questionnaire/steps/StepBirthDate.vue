5661d9091778efc4a8cccc727e6cca2a994b029e
Kubatron 117 <kubatron117@gmail.com>
2025-02-17 14:36:20 +0100

    i18n

--- src/components/questionnaire/steps/StepBirthDate.vue
+++ src/components/questionnaire/steps/StepBirthDate.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="max-w-md mx-auto my-10 p-6 bg-white rounded-xl shadow-lg">
     <h2 class="text-2xl font-semibold text-gray-800 mb-6 text-center">
-      Zadejte datum narození
+      {{ t('app.steps.insert-date-of-birth') }}
     </h2>
     <div class="space-y-6">
       <div>
         <label for="birthDate" class="block text-gray-700 font-medium mb-2">
-          Datum narození:
+          {{ t('app.steps.date-of-birth') }}:
         </label>
         <DatePicker
           v-model="localBirthDate"
           inputId="birthDate"
           dateFormat="dd.mm.yy"
           :maxDate="today"
+          :manualInput="true"
           showIcon
           class="w-full p-2 border border-gray-300 rounded-md"
         />
@@ -25,6 +26,7 @@
 import { ref, watch } from 'vue';
 import DatePicker from 'primevue/datepicker';
 import { useQuestionnaireStore } from '@/stores/questionnaireStore';
+import { useI18n } from 'vue-i18n'
 
 const questionnaireStore = useQuestionnaireStore();
 const localBirthDate = ref(questionnaireStore.birthDate);
@@ -32,6 +34,8 @@ const localBirthDate = ref(questionnaireStore.birthDate);
 watch(localBirthDate, (newVal) => {
   questionnaireStore.birthDate = newVal;
 });
+
+const { t } = useI18n()
 
 const today = new Date();
 </script>
