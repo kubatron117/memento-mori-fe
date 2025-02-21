a39039eddc0c3601fdf8e0953cecb4a87d3cf419
Kubatron 117 <kubatron117@gmail.com>
2025-02-21 13:51:05 +0100

    Tool tips

--- src/components/questionnaire/steps/StepFinal.vue
+++ src/components/questionnaire/steps/StepFinal.vue
@@ -8,36 +8,78 @@
       <h3 class="text-lg font-semibold mb-2">Sumarizace výsledků</h3>
       <ul class="list-disc pl-5 text-gray-700">
         <li>
-          <span>Základní očekávaná délka života: </span>
-          <strong>{{ roundedBaseLifeExpectancy }} let</strong>
+          <div class="flex items-center gap-x-1">
+            <span>Datum narození: </span>
+            <strong class="">{{ formatDate(questionnaireStore?.birthDate) }}</strong>
+          </div>
         </li>
         <li>
-          <span>Přínos fyzické aktivity: </span>
-          <strong class="text-green-600">+{{ roundedActivityLifeGain }} let</strong>
+          <div class="flex items-center gap-x-1">
+            <span>Váš aktuální věk: </span>
+            <strong class="">{{ questionnaireStore.getCurrentAge() }} let</strong>
+          </div>
         </li>
         <li>
-          <span>Přínos diety: </span>
-          <strong class="text-green-600">+{{ roundedDietLifeGain }} let</strong>
+          <div class="flex items-center gap-x-1">
+            <span>Základní očekávaná délka života: </span>
+            <strong>{{ roundedBaseLifeExpectancy }} let</strong>
+            <i
+              class="pi pi-info-circle ml-1 cursor-pointer"
+              v-tooltip="t('app.steps.final.tooltips.baseLifeExpectancy')"
+            ></i>
+          </div>
         </li>
         <li>
-          <span>Ztráta vlivem alkoholu: </span>
-          <strong class="text-red-600">-{{ roundedAlcoholLifeLoss }} let</strong>
+          <div class="flex items-center gap-x-1">
+            <span>Přínos vlivem fyzické aktivity: </span>
+            <strong class="text-green-600">+{{ roundedActivityLifeGain }} let</strong>
+            <i
+              class="pi pi-info-circle ml-1 cursor-pointer"
+              v-tooltip="t('app.steps.final.tooltips.physicalActivity')"
+            ></i>
+          </div>
         </li>
         <li>
-          <span>Ztráta vlivem kouření: </span>
-          <strong class="text-red-600">-{{ roundedSmokingLossYears }} let</strong>
+          <div class="flex items-center gap-x-1">
+            <span>Přínos vlivem stravy: </span>
+            <strong class="text-green-600">+{{ roundedDietLifeGain }} let</strong>
+            <i
+              class="pi pi-info-circle ml-1 cursor-pointer"
+              v-tooltip="t('app.steps.final.tooltips.food')"
+            ></i>
+          </div>
         </li>
         <li>
-          <span>Další ztráty z kouření: </span>
-          <strong class="text-red-600">-{{ roundedAdditionalSmokingLossYears }} let</strong>
+          <div class="flex items-center gap-x-1">
+            <span>Ztráta vlivem alkoholu: </span>
+            <strong class="text-red-600">-{{ roundedAlcoholLifeLoss }} let</strong>
+            <i
+              class="pi pi-info-circle ml-1 cursor-pointer"
+              v-tooltip="t('app.steps.final.tooltips.alcohol')"
+            ></i>
+          </div>
         </li>
         <li>
-          <span>Datum narození: </span>
-          <strong class="">{{ formatDate(questionnaireStore?.birthDate) }}</strong>
+          <div class="flex items-center gap-x-1">
+            <span>Ztráta vlivem dosavadního kouření: </span>
+            <strong class="text-red-600">-{{ roundedSmokingLossYears }} let</strong>
+            <i
+              class="pi pi-info-circle ml-1 cursor-pointer"
+              v-tooltip="t('app.steps.final.tooltips.smokingLoss')"
+            ></i>
+          </div>
         </li>
         <li>
-          <span>Váš aktuální věk: </span>
-          <strong class="">{{ questionnaireStore.getCurrentAge() }} let</strong>
+          <div class="flex items-center gap-x-1">
+            <span>Ztráta vlivem budoucího kouření: </span>
+            <strong class="text-red-600">-{{ roundedAdditionalSmokingLossYears }} let</strong>
+          </div>
+        </li>
+        <li>
+          <div class="flex items-center gap-x-1">
+            <span>Očekávaná délka života ovlivněna faktory: </span>
+            <strong>{{ roundedExpectedLifeExpectancy }} let</strong>
+          </div>
         </li>
       </ul>
     </div>
@@ -70,12 +112,16 @@
 import { computed, ref, watch } from 'vue';
 import Slider from 'primevue/slider';
 import { useQuestionnaireStore } from '@/stores/questionnaireStore';
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
 
 const questionnaireStore = useQuestionnaireStore();
 
 const roundOneDecimal = (num: number) => Math.round(num * 10) / 10;
 
 const roundedBaseLifeExpectancy = computed(() => roundOneDecimal(questionnaireStore.baseLifeExpectancyForGender));
+const roundedExpectedLifeExpectancy = computed(() => roundOneDecimal(questionnaireStore.finalLifeExpectancy));
 const roundedActivityLifeGain = computed(() => roundOneDecimal(questionnaireStore.activityLifeGain));
 const roundedDietLifeGain = computed(() => roundOneDecimal(questionnaireStore.dietLifeGain));
 const roundedAlcoholLifeLoss = computed(() => roundOneDecimal(questionnaireStore.alcoholLifeLoss));
