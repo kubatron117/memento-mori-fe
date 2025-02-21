85d4b75e0a46bde87dec67f07262548fa8fe15a8
Kubatron 117 <kubatron117@gmail.com>
2025-02-21 10:02:57 +0100

    Updated text for physical Activity

--- src/components/questionnaire/steps/StepPhysicalActivity.vue
+++ src/components/questionnaire/steps/StepPhysicalActivity.vue
@@ -8,7 +8,33 @@
       {{ t('app.steps.physical-activity.exerciseType') }}
     </p>
 
-    <div class="flex flex-col sm:flex-row sm:space-x-6 space-y-4 sm:space-y-0">
+    <div class="mt-4 p-4 border border-gray-200 rounded bg-gray-50">
+      <h3 class="text-lg font-semibold text-gray-800 mb-2">
+        Definice intenzity cvičení
+      </h3>
+      <ul class="list-disc ml-6 text-gray-700">
+        <li>
+          <span class="font-bold">Mírná:</span>
+          Aktivita o intenzitě 3–5,9 MET (např. Chůze rychlým tempem (4,8–6,5 km/h), pomalá jízda na kole (15 km/h), malování/dekorování, vysávání, zahradničení (sekání trávníku), golf (tažení holí ve vozíku), tenis (dvojice), společenský tanec a vodní aerobik.). Dýchání je zrychlené, ale umožňuje mluvit v celých větách.
+        </li>
+        <li>
+          <span class="font-bold">Intenzivní:</span>
+          Aktivita s hodnotou ≥6 MET (např. Závodní chůze, jogging nebo běh, jízda na kole rychlostí nad 15 km/h, náročné zahradničení (nepřetržité kopání nebo orání), plavání bazénem a tenis (singl)). Dýchání je velmi rychlé a mluvení je obtížné.
+        </li>
+      </ul>
+    </div>
+
+    <div class="flex flex-col sm:flex-row sm:space-x-6 space-y-4 sm:space-y-0 mt-6">
+      <div class="flex items-center">
+        <RadioButton
+          v-model="activityStore.exerciseType"
+          value="none"
+          inputId="none"
+        />
+        <label for="none" class="ml-2 cursor-pointer text-gray-700">
+          {{ t('app.steps.physical-activity.noActivityLabel') }}
+        </label>
+      </div>
       <div class="flex items-center">
         <RadioButton
           v-model="activityStore.exerciseType"
@@ -29,16 +55,6 @@
         <label for="vigorous" class="ml-2 cursor-pointer text-gray-700">
           {{ t('app.steps.physical-activity.vigorousLabel') }}
           <span class="text-xs text-gray-500"> (např. běh)</span>
-        </label>
-      </div>
-      <div class="flex items-center">
-        <RadioButton
-          v-model="activityStore.exerciseType"
-          value="none"
-          inputId="none"
-        />
-        <label for="none" class="ml-2 cursor-pointer text-gray-700">
-          {{ t('app.steps.physical-activity.noActivityLabel') }}
         </label>
       </div>
     </div>
@@ -68,7 +84,6 @@
     </div>
   </div>
 </template>
-
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
