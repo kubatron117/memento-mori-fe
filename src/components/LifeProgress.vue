dbe8cde737d45b1ca460d47abb516d48aa57e9b2
Kubatron 117 <kubatron117@gmail.com>
2025-02-25 10:00:54 +0100

    Component - after expected days

--- src/components/LifeProgress.vue
+++ src/components/LifeProgress.vue
@@ -5,7 +5,7 @@
         {{ t('app.life.percentageOfLifeLived') }}
       </h4>
       <ProgressBar :value="percentage" class="w-full" style="height: 1rem;" />
-      <div class="mt-4 flex justify-between text-base">
+      <div v-if="!isExceeded" class="mt-4 flex justify-between text-base">
         <p>
           {{ t('app.life.daysLived') }}:
           <span class="font-semibold">{{ daysLived }}</span>
