3fccebc04398959430d877071c46ad4f8fddff3a
Kubatron 117 <kubatron117@gmail.com>
2025-02-28 10:31:31 +0100

    / redirect show WeeksInLifeView

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,7 +17,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: WeeksInLifeView,
     },
     {
       path: '/weeks-in-life',
